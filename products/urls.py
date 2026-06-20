d29a6c91aeb3e4aee083edae37c6c5dd4f5e0461
Md Habibur Rahman <habibur.cse.diu@gmail.com>
2026-06-20 12:49:56 +0600

    product detail page added

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
+    # Product details page route
+    path('medicine/<slug:slug>/', views.medicine_detail, name='medicine_detail'), 
+    
     # Category-wise product list page (Class-based View)
     path('<slug:category_slug>/', views.CategoryProductListView.as_view(), name='category_products'),
     
-    # Product details page route
-    path('medicine/<slug:slug>/', views.medicine_detail, name='medicine_detail'), 
+    
 ]
